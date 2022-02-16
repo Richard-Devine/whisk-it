4a7b92687de8890c190b6bc4c2bda3dbeb80f92a
Richard Devine <richard_devine@outlook.com>
2022-02-16 08:04:40 +0000

    initial

---
+++ whisk-it/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom';
+import './index.css';
+import LandingPage from './landing-page/landing-page';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LandingPage/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
