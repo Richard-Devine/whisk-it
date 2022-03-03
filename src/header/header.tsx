c3ff94258b7a9822af03a104b8b11d30534d19fb
Richard <richard_devine@outlook.com>
2022-03-03 18:34:05 +0000

    images and css

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import {HeaderProps} from "../webpage-types";
 import Menu from "../menu/menu";
+import whiskItLogo from '../images/logo croped.jpg'
 
 export default class Header extends React.Component <HeaderProps> {
     render() {
         return(
             <div>
-                <div data-testid='logo-container'>
-                    <img src="" alt="Whisk It logo"/>
+                <div data-testid='logo-container' className='header-banner-div'>
+                    <img src={whiskItLogo} alt="Whisk It logo" className='header-banner-image'/>
                 </div>
                 <div data-testid='menu-container'>
                     <Menu onClick={(x) => this.props.onClick(x)}/>
