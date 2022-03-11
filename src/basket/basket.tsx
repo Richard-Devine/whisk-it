a46a7a6c433b3d90aad9760b6ca3553b943603cb
Richard <richard_devine@outlook.com>
2022-03-11 19:35:31 +0000

    css

--- src/basket/basket.tsx
+++ src/basket/basket.tsx
@@ -3,32 +3,36 @@ import {BasketProps} from '../webpage-types'
 
 export default class Basket extends React.Component<BasketProps> {
 
-    removeFromBasket(i:number) {
+    removeFromBasket(i: number) {
         this.props.myBasket.splice(i, 1)
         this.forceUpdate()
     }
 
     render() {
         return (
-            <div>
+            <div className='basket-wrapper'>
+                <div>
                 {this.props.myBasket.map((product, i) => {
-                    return(
-                <div key={i} data-testid='basket-wrapper'>
-                    <div data-testid='image-div'>
-                        <img src={product.imageURL} alt={product.title}/>
-                    </div>
-                    <div data-testid='delete-button-div' onClick={() => this.removeFromBasket(i)}>
-                        X
-                    </div>
-                    <div data-testid='title-div'>
-                        {product.title}
-                    </div>
-                    <div data-testid='price-div'>
-                        {product.price}
-                    </div>
-                </div>
+                    return (
+                        <div key={i} data-testid='basket-items-wrapper' className='basket-items-wrapper'>
+                            <div data-testid='image-div' className='image-div'>
+                                <img src={product.imageURL} alt={product.title}/>
+                            </div>
+                            <div className='basket-content-wrapper' data-testid='basket-content-wrapper'>
+                                <span data-testid='title-div'>
+                                    {product.title}
+                                </span>
+                                <span data-testid='delete-button-div' onClick={() => this.removeFromBasket(i)}>
+                                    X
+                                </span>
+                                <div data-testid='price-div'>
+                                    {product.price}
+                                </div>
+                            </div>
+                        </div>
                     )
                 })}
+                </div>
                 <div data-testid='buy-div'>
                     Buy Via Paypal
                 </div>
@@ -36,3 +40,5 @@ export default class Basket extends React.Component<BasketProps> {
         )
     }
 }
+
+//TODO Basket tests for new elements
