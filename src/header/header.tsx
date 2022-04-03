c0f37e1d3239573ce915e5434a2fd96a68a031d4
Richard <richard_devine@outlook.com>
2022-04-03 10:37:48 +0100

    css @media

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import {HeaderProps} from "../webpage-types";
 import Menu from "../menu/menu";
 import whiskItLogo from "../website-icons/Whisk it banner transparent.webp";
+import {Link} from "react-router-dom";
 
 export default class Header extends React.Component <HeaderProps> {
     render() {
         return (
             <div>
                 <div data-testid="logo-container" className="header-banner-div">
-                    <img src={whiskItLogo} alt="Whisk It logo" className="header-banner-image"/>
+                   <Link to="/"><img src={whiskItLogo} alt="Whisk It logo" className="header-banner-image"/></Link>
                 </div>
                 <div data-testid="menu-container" className="menu-container">
                     <Menu myBasket={this.props.myBasket}/>
